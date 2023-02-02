bae31178e6bfd82d26d197169e2fe16fc55d383f
liam wilson <124217020+soundboardman@users.noreply.github.com>
2023-02-02 02:48:34 +0000

    lol

---
+++ script.js
@@ -0,0 +1,8 @@
+.large-button {
+    width: 200px;
+    height: 190px;
+    position: absolute;
+    background: url(/media/images/transparent_button_normal.png.pagespeed.ce._PQuVSQuHh.png) no-repeat;
+    border: 0;
+    display: block;
+    -webkit-tap-highlight-color: transparent;
